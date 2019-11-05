d7adfe03f0072a7d93ecc60a788466fafa19909e
Lovecraftian Horror <LovecraftianHorror@pm.me>
2019-11-05 00:18:41 -0500

    Allow pytest invocation in top level

--- tests/test_subwinder.py
+++ tests/test_subwinder.py
@@ -5,15 +5,16 @@ import os
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
 
 
 def test_special_hash():
-    hashes = {
-        "random_1": "38516a7d01f4e37d",
-        "random_2": "a16ad3dbbe8037fa"
-    }
+    # Maps filename to hash
+    hashes = {"random_1": "38516a7d01f4e37d", "random_2": "a16ad3dbbe8037fa"}
 
+    tests_dir = os.path.dirname(__file__)
     for file, ideal_hash in hashes.items():
-        hash = subwinder.utils.special_hash(os.path.join("hash_files", file))
+        hash = subwinder.utils.special_hash(
+            os.path.join(tests_dir, "hash_files", file)
+        )
         assert hash == ideal_hash
