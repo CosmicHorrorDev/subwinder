6fa4447e9f70d7bb241a7a8bc8c10db7af63b503
Lovecraftian Horror <LovecraftianHorror@pm.me>
2019-11-06 18:18:04 -0500

    Separate tests and test 'AuthSubWinder' raising auth errors

--- tests/test_subwinder.py
+++ tests/test_subwinder.py
@@ -1,37 +1,24 @@
 from subwinder import __version__
-import subwinder.hashers
+from subwinder.exceptions import SubAuthError
+from subwinder.lib import AuthSubWinder
 
-import os
+import pytest
 
 
 def test_version():
     assert __version__ == "0.1.0"
 
 
-def test_special_hash():
-    # Maps filename to hash
-    hashes = {"random_1": "38516a7d01f4e37d", "random_2": "a16ad3dbbe8037fa"}
+def test_authsubwinder_init():
+    bad_params = [
+        # Missing both username and password
+        ["<useragent"],
+        # Missing password
+        ["<useragent>", "<username>"],
+        # Invalid useragent
+        [None, "<username>", "<password>"],
+    ]
 
-    tests_dir = os.path.dirname(__file__)
-    for file, ideal_hash in hashes.items():
-        hash = subwinder.hashers.special_hash(
-            os.path.join(tests_dir, "hash_files", file)
-        )
-        assert hash == ideal_hash
-
-
-def test_md5_hash():
-    """
-    Same concept as `test_special_hash`, but with md5 hash instead
-    """
-    hashes = {
-        "random_1": "52b16cb071a2da214782380a0100003b",
-        "random_2": "f6eef93deb14c19ded05f76a17928b21",
-    }
-
-    tests_dir = os.path.dirname(__file__)
-    for file, ideal_hash in hashes.items():
-        hash = subwinder.hashers.md5_hash(
-            os.path.join(tests_dir, "hash_files", file)
-        )
-        assert hash == ideal_hash
+    for params in bad_params:
+        with pytest.raises(SubAuthError):
+            AuthSubWinder(*params)
