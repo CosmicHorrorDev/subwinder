4ca09e1e324e47ec8e1415428dfd1f3c2c2a4c39
Lovecraftian Horror <LovecraftianHorror@pm.me>
2019-11-06 17:50:03 -0500

    Add test for 'md5_hash' function

--- tests/test_subwinder.py
+++ tests/test_subwinder.py
@@ -18,3 +18,20 @@ def test_special_hash():
             os.path.join(tests_dir, "hash_files", file)
         )
         assert hash == ideal_hash
+
+
+def test_md5_hash():
+    """
+    Same concept as `test_special_hash`, but with md5 hash instead
+    """
+    hashes = {
+        "random_1": "52b16cb071a2da214782380a0100003b",
+        "random_2": "f6eef93deb14c19ded05f76a17928b21",
+    }
+
+    tests_dir = os.path.dirname(__file__)
+    for file, ideal_hash in hashes.items():
+        hash = subwinder.hashers.md5_hash(
+            os.path.join(tests_dir, "hash_files", file)
+        )
+        assert hash == ideal_hash
