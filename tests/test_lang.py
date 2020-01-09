087f21289957e391791fbbb3a121e189e2fe296e
Lovecraftian Horror <LovecraftianHorror@pm.me>
2020-01-09 14:10:04 -0500

    Add test for '_LangConverter'

---
+++ tests/test_lang.py
@@ -0,0 +1,50 @@
+from datetime import timedelta
+from unittest.mock import call, patch
+
+from subwinder.base import SubWinder
+from subwinder.lang import _LangConverter, LangFormat
+
+RESP = [
+    {"ISO639": "de", "SubLanguageID": "ger", "LanguageName": "German"},
+    {"ISO639": "en", "SubLanguageID": "eng", "LanguageName": "English"},
+    {"ISO639": "fr", "SubLanguageID": "fre", "LanguageName": "French"},
+]
+
+
+def test_LangConverter():
+    converter = _LangConverter()
+    assert converter._last_updated is None
+
+    with patch.object(
+        SubWinder, "_get_languages", return_value=RESP
+    ) as mocked:
+        # After first update converter shouldn't request again for an hour
+        converter._update()
+
+        # These will not call `_get_languages`
+        converter._update()
+        assert "eng" == converter.convert(
+            "en", LangFormat.LANG_2, LangFormat.LANG_3
+        )
+        assert "English" == converter.convert(
+            "eng", LangFormat.LANG_3, LangFormat.LANG_LONG
+        )
+        assert ["de", "en", "fr"] == converter.list(LangFormat.LANG_2)
+        assert ["ger", "eng", "fre"] == converter.list(LangFormat.LANG_3)
+        assert ["German", "English", "French"] == converter.list(
+            LangFormat.LANG_LONG
+        )
+
+        # `_get_languages` should only be called once
+        mocked.assert_called_once_with()
+
+        # Now wait long enough that we will refresh the langs
+        converter._last_updated -= timedelta(seconds=3600)
+        converter.list(LangFormat.LANG_2)
+        mocked.assert_has_calls([call(), call()])
+
+
+# TODO: in this one test out lang_2s, lang_3s, and lang_longs, make sure
+#       request is only run once even though all the globals should get updated
+def test_globals():
+    pass
