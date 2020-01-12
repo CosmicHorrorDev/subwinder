9bb2ca39ffa90c2502ab1855e8ae425706f197bc
Lovecraftian Horror <LovecraftianHorror@pm.me>
2020-01-12 15:05:21 -0500

    Add test for 'extract'

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,17 @@
 import pytest
 
+from subwinder.utils import extract
 
-@pytest.mark.skip()
+
 def test_extract():
-    pass
+    COMPRESSED = (
+        "H4sIAIXHxV0C/yXLwQ0CMQxE0VbmxoVCoAyzHiBS4lnFXtB2TyRuT/r6N/Yu1JuTV9wvY"
+        "9EKL8mhTmwa+2QmHRYOxiZfzuNRrVZv8dQcVk3xP08dSMFps5/4WhRKSPvwBzf2OXZqAA"
+        "AA"
+    )
+    IDEAL = (
+        "Hello there, I'm that good ole compressed and encoded subtitle"
+        " information that you so dearly want to save"
+    )
+
+    assert extract(COMPRESSED, "UTF-8") == IDEAL
