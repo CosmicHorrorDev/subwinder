602290ebe56b5f5e48c597f759504fdba8d7f238
Lovecraftian Horror <LovecraftianHorror@pm.me>
2020-01-13 09:15:46 -0500

    Implement 'from_data' for data classes

--- subwinder/info.py
+++ subwinder/info.py
@@ -27,10 +27,14 @@ class UserInfo:
     nickname: str
 
 
-# TODO: switch this over to the `from_data` style? Would make setting up the
-#       test easier
 @dataclass
 class FullUserInfo(UserInfo):
+    rank: str
+    uploads: int
+    downloads: int
+    preferred_languages: list
+    web_language: str
+
     def __init__(self, data):
         super().__init__(data["IDUser"], data["UserNickName"])
         self.rank = data["UserRank"]
@@ -48,8 +52,6 @@ class FullUserInfo(UserInfo):
         self.web_language = data["UserWebLanguage"]
 
 
-# TODO: switch this over to the `from_data` style? Would make setting up the
-#       test easier
 @dataclass
 class Comment:
     author: UserInfo
@@ -92,10 +94,8 @@ class TvSeriesInfo(MediaInfo):
 
 @dataclass
 class EpisodeInfo(TvSeriesInfo):
-    def __init__(self, name, year, imdbid, season, episode, dirname, filename):
-        super().__init__(name, year, imdbid, dirname, filename)
-        self.season = season
-        self.episode = episode
+    season: int
+    episode: int
 
     @classmethod
     def from_data(cls, data, dirname, filename):
@@ -119,10 +119,22 @@ class EpisodeInfo(TvSeriesInfo):
         )
 
 
-# TODO: include the full language in here as well?
 @dataclass
 class SubtitlesInfo:
-    def __init__(self, data):
+    size: int
+    downloads: int
+    num_comments: int
+    rating: float
+    id: str
+    file_id: str
+    filename: str
+    lang_2: str
+    lang_3: str
+    ext: str
+    encoding: str
+
+    @classmethod
+    def from_data(self, data):
         self.size = int(data["SubSize"])
         self.downloads = int(data["SubDownloadsCnt"])
         self.num_comments = int(data["SubComments"])
--- tests/test_results.py
+++ tests/test_results.py
@@ -1,6 +1,36 @@
 import pytest
 
+from datetime import datetime
+import json
+import os
 
-@pytest.mark.skip()
+from subwinder.info import EpisodeInfo, SubtitlesInfo, UserInfo
+from subwinder.results import SearchResult
+from tests.constants import SAMPLES_DIR
+
+
 def test_SearchResult():
-    pass
+    with open(os.path.join(SAMPLES_DIR, "search_subtitles.json")) as f:
+        SAMPLE_RESP = json.load(f)
+
+    # TODO: implement from_data for here
+    sr = SearchResult.from_data(SAMPLE_RESP, "dir", "file")
+    assert sr.author == UserInfo("1332962", "elderman")
+    assert sr.media == EpisodeInfo(
+        '"Fringe" Alone in the World', 2011, "1998676", 4, 3, "dir", "file"
+    )
+    # TODO: add from_data here
+    assert sr.subtitles == SubtitlesInfo(
+        58024,
+        57765,
+        0,
+        7.9,
+        "4251071",
+        "1952941557",
+        "Fringe.S04E03.HDTV.XviD-LOL.srt",
+        "en",
+        "eng",
+        "srt",
+        "UTF-8",
+    )
+    assert sr.upload_date == datetime(2011, 10, 8, 7, 36, 1)
