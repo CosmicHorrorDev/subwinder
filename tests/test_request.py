b45e53deac10c7ff3498599e19ed5e16e36b5af3
Lovecraftian Horror <LovecraftianHorror@pm.me>
2020-01-13 10:15:59 -0500

    Add test for '_request'

--- tests/test_request.py
+++ tests/test_request.py
@@ -1,6 +1,25 @@
-import pytest
+from unittest.mock import call, patch
+
+from subwinder.request import _client, _request
 
 
-@pytest.mark.skip()
+# Note: this test takes a bit of time because of the delayed API request retry
 def test__request():
-    pass
+    RESP = {"status": "200 OK", "data": "The data!", "seconds": "0.15"}
+    with patch.object(_client, "ServerInfo", return_value=RESP) as mocked:
+        # Response should be passed through on success
+        assert _request("ServerInfo", None) == RESP
+        mocked.assert_called_once_with()
+
+    CALLS = [
+        call("<token>", "arg1", "arg2"),
+        call("<token>", "arg1", "arg2"),
+    ]
+    RESPS = [
+        {"status": "429 Too many requests", "seconds": "0.10"},
+        {"status": "200 OK", "data": "The data!", "seconds": "0.15"},
+    ]
+    with patch.object(_client, "GetUserInfo") as mocked:
+        mocked.side_effect = RESPS
+        assert _request("GetUserInfo", "<token>", "arg1", "arg2") == RESPS[1]
+        mocked.assert_has_calls(CALLS)
