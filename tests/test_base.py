751595290ad6909f76503f3d4288ec3b973a006b
Lovecraftian Horror <LovecraftianHorror@pm.me>
2020-01-14 00:31:09 -0500

    Added test for 'get_languages'

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,18 +1,33 @@
 import pytest
 
+from datetime import datetime
+
 from subwinder.base import SubWinder
+from subwinder.lang import _converter
 
 
-@pytest.mark.skip()
+LANGS = [
+    ("de", "ger", "German"),
+    ("en", "eng", "English"),
+    ("fr", "fre", "French"),
+]
+# Fake already updated langs
+_converter._langs = LANGS
+_converter._last_updated = datetime.now()
+
+
+@pytest.mark.skip(reason="Only passes the values onto `_request`")
 def test__request():
     pass
 
 
-@pytest.mark.skip()
 def test_get_languages():
-    pass
+    sw = SubWinder()
+    assert sw.get_languages() == LANGS
 
 
-@pytest.mark.skip()
+@pytest.mark.skip(
+    reason="Currently just calls the API, and method is likely to change"
+)
 def test_server_info():
     pass
