9ade957215c8f8ae4628046d00273a44237239e9
Lovecraftian Horror <LovecraftianHorror@pm.me>
2020-04-28 10:44:15 -0400

    Forgot to add new files

---
+++ subwinder/_ranking.py
@@ -0,0 +1,25 @@
+def rank_guess_media(results, query):
+    return results["BestGuess"]
+
+
+def rank_search_subtitles(results, query, exclude_bad=True, sub_exts=None):
+    best_result = None
+    max_score = None
+
+    # Force list of `sub_exts` to be lowercase
+    sub_exts = None if sub_exts is None else [ext.lower() for ext in sub_exts]
+
+    for result in results:
+        # Skip if someone listed sub as bad and `exclude_bad` is `True`
+        if exclude_bad and result["SubBad"] != "0":
+            continue
+
+        # Skip incorrect `sub_ext`s if provided
+        if sub_exts is not None and result["SubFormat"].lower() not in sub_exts:
+            continue
+
+        if max_score is None or result["Score"] > max_score:
+            best_result = result
+            max_score = result["Score"]
+
+    return best_result
---
+++ subwinder/_request.py
@@ -0,0 +1,109 @@
+from datetime import datetime
+import time
+from xmlrpc.client import ServerProxy, Transport, ProtocolError
+
+from subwinder.constants import _API_BASE, _REPO_URL
+from subwinder.exceptions import (
+    SubAuthError,
+    SubDownloadError,
+    SubLibError,
+    SubServerError,
+    SubUploadError,
+)
+
+
+# Responses 403, 404, 405, 406, 409 should be prevented by API
+_API_ERROR_MAP = {
+    "401": SubAuthError,
+    "402": SubUploadError,
+    "407": SubDownloadError,
+    "408": SubLibError,
+    "410": SubLibError,
+    "411": SubAuthError,
+    "412": SubUploadError,
+    "413": SubLibError,
+    "414": SubAuthError,
+    "415": SubAuthError,
+    "416": SubUploadError,
+    "429": SubServerError,
+    "503": SubServerError,
+    "506": SubServerError,
+    "520": SubServerError,
+}
+
+_API_PROTOCOL_ERR_MAP = {
+    503: "503 Service Unavailable",
+    506: "506 Server under maintenance",
+    520: "520 Unknown internal error",
+}
+
+_client = ServerProxy(_API_BASE, allow_none=True, transport=Transport())
+
+
+# TODO: give a way to let lib user to set `TIMEOUT`?
+def request(method, token, *params):
+    TIMEOUT = 15
+    DELAY_FACTOR = 2
+    current_delay = 1.5
+    start = datetime.now()
+
+    # Keep retrying if status code indicates rate limiting (429) or server error (5XX)
+    # until the `TIMEOUT` is hit
+    while True:
+        try:
+            if method in ("AutoUpdate", "GetSubLanguages", "LogIn", "ServerInfo"):
+                # Flexible way to call method while reducing error handling
+                resp = getattr(_client, method)(*params)
+            else:
+                # Use the token if it's defined
+                resp = getattr(_client, method)(token, *params)
+        except ProtocolError as err:
+            # Try handling the `ProtocolError` appropriately
+            if err.errcode in _API_PROTOCOL_ERR_MAP:
+                resp = {"status": _API_PROTOCOL_ERR_MAP[err.errcode]}
+            else:
+                # Unexpected `ProtocolError`
+                raise SubLibError(
+                    "The server returned an unhandled protocol error. Please raise an"
+                    f" issue in the repo ({_REPO_URL}) so that this can be handled in"
+                    f" the future\nProtocolError: {err}"
+                )
+
+        # All requests return a status except for GetSubLanguages and ServerInfo so
+        # force the status
+        if method in ("GetSubLanguages", "ServerInfo"):
+            resp["status"] = "200 OK"
+
+        if "status" not in resp:
+            raise SubLibError(
+                f'"{method}" should return a status and didn\'t, consider raising an'
+                f" issue at {_REPO_URL} to address this"
+            )
+
+        status_code = resp["status"][:3]
+        status_msg = resp["status"][4:]
+
+        # Retry if rate limit was hit (429) or server error (5XX), otherwise handle
+        # appropriately
+        if status_code not in ("429", "503", "506", "520"):
+            break
+
+        # Server under heavy load, wait and retry
+        remaining_time = TIMEOUT - (datetime.now() - start).total_seconds()
+        if remaining_time <= current_delay:
+            # Not enough time to try again so go ahead and `break`
+            break
+
+        time.sleep(current_delay)
+        current_delay *= DELAY_FACTOR
+
+    # Handle the response
+    if status_code == "200":
+        return resp
+    elif status_code in _API_ERROR_MAP:
+        raise _API_ERROR_MAP[status_code](status_msg)
+    else:
+        raise SubLibError(
+            "the API returned an unhandled response, consider raising an issue to"
+            f" address this at {_REPO_URL}\nResponse status: '{resp['status']}'"
+        )
