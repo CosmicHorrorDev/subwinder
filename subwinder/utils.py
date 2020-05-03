6b5821045e92d4c6af365dd2416de5ef41e2954a
Lovecraftian Horror <LovecraftianHorror@pm.me>
2020-05-03 03:37:42 -0400

    Try to follow other hashes a bit more

--- subwinder/utils.py
+++ subwinder/utils.py
@@ -13,12 +13,13 @@ def extract(bytes, encoding):
 # https://trac.opensubtitles.org/projects/opensubtitles/wiki/HashSourceCodes
 def special_hash(filepath):
     CHUNK_SIZE_BYTES = 64 * 1024
-    HASH_SIZE_BYTES = 8
+    # HASH_SIZE_BYTES = 8
     FILE_MIN_SIZE = CHUNK_SIZE_BYTES * 2
 
     with filepath.open("rb") as f:
         filesize = filepath.stat().st_size
-        filehash = filesize
+        # filehash = filesize
+        hasher = _SumHasher(filesize)
 
         if filesize < FILE_MIN_SIZE:
             raise SubHashError(f"Filesize is below minimum of {FILE_MIN_SIZE} bytes")
@@ -28,11 +29,25 @@ def special_hash(filepath):
             if i == 1:
                 f.seek(-CHUNK_SIZE_BYTES, 2)
 
-            # Read the first 64 KiB summing every 8 bytes
-            for _ in range(CHUNK_SIZE_BYTES // HASH_SIZE_BYTES):
-                buffer = f.read(HASH_SIZE_BYTES)
-                filehash += int.from_bytes(buffer, byteorder="little")
+            # Process each chunk of the file
+            chunk = f.read(CHUNK_SIZE_BYTES)
+            hasher.update(chunk)
 
-    # Keep output in `HASH_SIZE_BYTES`
-    filehash &= 2 ** (HASH_SIZE_BYTES * 8) - 1
-    return f"{filehash:016x}"
+    return hasher.digest()
+
+
+class _SumHasher:
+    def __init__(self, filesize):
+        self.hash = filesize
+        self.digest_size = 8
+        self.block_size = 8
+
+    def update(self, values):
+        for i in range(0, len(values), self.block_size):
+            chunk = values[i : i + self.block_size]
+            self.hash += int.from_bytes(chunk, byteorder="little")
+
+    def digest(self):
+        # Keep output in `self.digest_size`
+        temp = self.hash & 2 ** (self.digest_size * 8) - 1
+        return f"{temp:016x}"
