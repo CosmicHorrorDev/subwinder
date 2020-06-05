1b102c58b5460746f5b4d8469f57df83c8da2562
Lovecraftian Horror <LovecraftianHorror@pm.me>
2020-06-05 15:12:09 -0400

    Don't skip marks when running specific marks

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,12 +16,13 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(autouse=True)
-def skip_non_default(request, pytestconfig):
+def skip_non_default(request):
     node = request.node
     run_all = request.config.getoption("--run-all")
+    mark = request.config.getoption("-m")
 
-    # Skip any tests that have a marker set
-    if not run_all and node.own_markers:
+    # Skip any tests with a marker set unless a marker or run all arg is passed in
+    if not mark and not run_all and node.own_markers:
         pytest.skip(
             f"Skipping mark for reason {node.own_markers[0].name}. Run with option"
             " --run-all to run this"
