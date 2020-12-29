4dcf0afb44b617b3360445fd25dff5328de88bb5
Lovecraftian Horror <LovecraftianHorror@pm.me>
2020-12-28 19:48:15 -0500

    Simplify adv_quickstart test

--- tests/examples_tests/test_examples.py
+++ tests/examples_tests/test_examples.py
@@ -1,6 +1,7 @@
 import filecmp
 import json
 import os
+import shutil
 import sys
 from pathlib import Path
 from unittest.mock import call, patch
@@ -12,7 +13,6 @@ from examples.advanced_quickstart import adv_quickstart
 from examples.interactive import interative
 from subwinder._constants import DEV_USERAGENT, Env
 from subwinder._request import Endpoints
-from subwinder.media import Media
 from tests.constants import EXAMPLES_ASSETS, EXAMPLES_RESPONSES
 
 USERNAME = "<username>"
@@ -69,11 +69,10 @@ def test_interactive(mock_request, tmp_path):
 
     # Set the fake responses
     with (EXAMPLES_RESPONSES / "interactive.json").open() as f:
-        RESPS = json.load(f)
-    mock_request.side_effect = RESPS
+        mock_request.side_effect = json.load(f)
 
+    # Set the fake user input
     with patch("builtins.input") as mock_input:
-        # Set the fake user input
         mock_input.side_effect = SAMPLE_INPUTS
 
         # Now that everything is setup let's test out the example
@@ -103,16 +102,28 @@ def test_interactive(mock_request, tmp_path):
 )
 @patch("subwinder._request.request")
 def test_adv_quickstart(mock_request, tmp_path):
-    # Setup all the values for our test
+    # Setup our ideal output directory
+    ideal_dir = tmp_path / "ideal"
+    ideal_input = ideal_dir / "Input"
+    ideal_output = ideal_dir / "Output"
+    shutil.copytree(EXAMPLES_ASSETS / "adv_quickstart", ideal_dir)
+
+    # Build up the dummy media since I don't want to keep it in the repo
+    fake_media(output_dir=ideal_input, entry_indicies=[5, 7, 8])
+    fake_media(output_dir=ideal_output, entry_indicies=[0, 1, 2, 3, 4, 6, 9, 10])
+
+    # Directory for our test to use
+    real_dir = tmp_path / "real"
+
+    # Setup all the values for our example function
     AUTHOR_WHITELIST = ["breathe"]
     SUB_EXTS = ["srt"]
     LANG = "en"
-    output_dir = tmp_path / "Output"
-    LEDGER = tmp_path / "ledger.json"
+    output_dir = real_dir / "Output"
+    LEDGER = real_dir / "ledger.json"
 
-    # Setup fake media in our input directory
-    input_dir = tmp_path / "Input"
-    input_dir.mkdir()
+    # Setup fake media in our test input directory
+    input_dir = real_dir / "Input"
     fake_media(output_dir=input_dir, entry_indicies=range(11))
 
     # Calls to verify against
@@ -158,8 +169,7 @@ def test_adv_quickstart(mock_request, tmp_path):
 
     # Set the fake responses
     with (EXAMPLES_RESPONSES / "adv_quickstart.json").open() as f:
-        RESPS = json.load(f)
-    mock_request.side_effect = RESPS
+        mock_request.side_effect = json.load(f)
 
     # Need to mock out glob here since the return order isn't guaranteed and I need it
     # to be consitent for testing. Because glob is a generator and I won't know the
@@ -183,79 +193,15 @@ def test_adv_quickstart(mock_request, tmp_path):
     # Verify the calls to `request` look good
     assert mock_request.call_args_list == CALLS
 
-    # Verify all the new files look right
-    adv_quickstart_assets = EXAMPLES_ASSETS / "adv_quickstart"
-    sub_files = [
-        Path("Output") / "Carnival of Souls (1962).eng.srt",
-        Path("Output") / "Detour (1945).eng.srt",
-        Path("Output") / "Fringe - s04e03 - Alone in the World.eng.srt",
-        Path("Output") / "McLintock (1963).eng.srt",
-        Path("Output") / "Night of the Living Dead (1968).eng.srt",
-        Path("Output") / "Night Watch (2004).eng.srt",
-        Path("Output") / "Plan 9 from Outer Space (1959).eng.srt",
-        Path("Output") / "The Last Man on Earth (1964).eng.srt",
-    ]
-    ledger_file = "ledger.json"
-
-    match, mismatch, errors = filecmp.cmpfiles(
-        tmp_path, adv_quickstart_assets, [*sub_files, ledger_file]
-    )
-    print(f"Mismatched: {mismatch}", file=sys.stderr)
-    print(f"Errors: {errors}", file=sys.stderr)
-    assert len(mismatch) == 0
-    assert len(errors) == 0
-
-    # And lastly verify that all the media still seems good (Would be easier if this was
-    # included in the repo, but I don't trust everything to treat it as sparse files)
-    # TODO: building up identical media in the temp dir is also an option and allows us
-    # to just compare the two different directories which could easily be worth it
-    no_match_params = [
-        ("dd68b108d270aa7b", 732924242, input_dir, "Algiers (1938).dummy"),
-        ("579bfcd8ee87dff3", 1529505156, input_dir, "Charade (1963).dummy"),
-        ("7d87bae932a2e533", 2632612813, input_dir, "Cyrano de Bergerac (1950).dummy"),
-    ]
-    match_params = [
-        ("6e02ec9fdf3c200f", 1251826386, output_dir, "Carnival of Souls (1962).dummy"),
-        ("c47cb30ab992fb34", 730435584, output_dir, "Detour (1945).dummy"),
-        (
-            "18379ac9af039390",
-            366876694,
-            output_dir,
-            "Fringe - s04e03 - Alone in the World.dummy",
-        ),
-        ("e1966a88db8f4a48", 948792230, output_dir, "McLintock (1963).dummy"),
-        (
-            "4f47a0266f3d15c5",
-            1743776999,
-            output_dir,
-            "Night of the Living Dead (1968).dummy",
-        ),
-        ("09a2c497663259cb", 733589504, output_dir, "Night Watch (2004).dummy"),
-        (
-            "b5a6939c71a6c3b6",
-            758756235,
-            output_dir,
-            "Plan 9 from Outer Space (1959).dummy",
-        ),
-        (
-            "2ef61c586962b462",
-            1322969681,
-            output_dir,
-            "The Last Man on Earth (1964).dummy",
-        ),
-    ]
-    no_match = [Media.from_parts(*params) for params in no_match_params]
-    match = [Media.from_parts(*params) for params in match_params]
-
-    for file in input_dir.glob("*.dummy"):
-        media = Media(file)
-        assert media in no_match
-        no_match.remove(media)
-
-    for file in output_dir.glob("*.dummy"):
-        media = Media(file)
-        assert media in match
-        match.remove(media)
-
-    assert len(no_match) == 0
-    assert len(match) == 0
+    # Verify the ideal and test directory is the same
+    cmp = filecmp.dircmp(real_dir, ideal_dir)
+    print(f"Left Only: {cmp.left_only}", file=sys.stderr)
+    print(f"Right Only: {cmp.right_only}", file=sys.stderr)
+    print(f"Common Funny: {cmp.common_funny}", file=sys.stderr)
+    print(f"Diff File: {cmp.diff_files}", file=sys.stderr)
+    print(f"Funny Files: {cmp.funny_files}", file=sys.stderr)
+    assert len(cmp.left_only) == 0
+    assert len(cmp.right_only) == 0
+    assert len(cmp.common_funny) == 0
+    assert len(cmp.diff_files) == 0
+    assert len(cmp.funny_files) == 0
