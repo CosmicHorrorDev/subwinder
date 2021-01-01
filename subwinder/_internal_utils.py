513d2b4d427c18e9650802bbf1564e284db9a68d
Lovecraftian Horror <LovecraftianHorror@pm.me>
2020-12-31 20:16:36 -0500

    Switch error type and improve error message

--- subwinder/_internal_utils.py
+++ subwinder/_internal_utils.py
@@ -1,7 +1,6 @@
 def type_check(obj, valid_classes):
     if not isinstance(obj, valid_classes):
-        # FIXME: this should be a typeerror not a valueerror
-        raise ValueError(
-            f"Expected `obj` to be type from {valid_classes}, but got type {type(obj)}"
-            " instead"
+        raise TypeError(
+            f"Expected `obj` to be type from {valid_classes} or a derived class, but"
+            f" got type {type(obj)} instead"
         )
